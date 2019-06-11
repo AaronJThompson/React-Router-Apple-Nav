de9be6b8ddc0a04669ce955a8ef37e1607d74164
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-11 13:47:33 +0100

    Add subScreens to mac object in preporation for SubNav

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -6,7 +6,18 @@ import NavWrapper from './components/Navigation/NavWrapper';
 
 let screens = [
   {path: '/', name: 'Home'},
-  {path: '/mac', name: 'Mac'},
+  {path: '/mac', name: 'Mac', subScreens:[
+    {path: '/macbook', name:'Macbook'},
+    {path: '/macbook-air', name:'Macbook Air'},
+    {path: '/macbook-pro', name:'Macbook Pro'},
+    {path: '/imac', name:'iMac'},
+    {path: '/imac-pro', name:'iMac Pro'},
+    {path: '/mac-pro', name:'Mac Pro'},
+    {path: '/mac/compare', name:'Compare'},
+    {path: '/pro-display-xdr', name:'Pro Display XDR'},
+    {path: '/mac/accessories', name:'Accessories'},
+    {path: '/macos/mojave', name:'Mojave'},
+  ]},
   {path: '/ipad', name: 'iPad'},
   {path: '/iphone', name: 'iPhone'},
   {path: '/watch', name: 'Watch'},

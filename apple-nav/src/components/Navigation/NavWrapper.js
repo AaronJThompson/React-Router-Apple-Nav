f0391a8a3dfb8aeef58a62e55f79252dfa0d518c
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-11 13:15:36 +0100

    Basic file structure

---
+++ apple-nav/src/components/Navigation/NavWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NavWrapper extends React.Component {
+    render() {
+        return (
+            <div>
+                I am the navigation
+            </div>
+        )
+    }
+}
